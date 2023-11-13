0ad3ee02f4938572ef1fcb44f9e7d9cd4f18f69d
mroach-esriuk <150164811+mroach-esriuk@users.noreply.github.com>
2023-11-13 18:28:49 +0000

    Deploying to gh-pages from @ EsriDevEvents/arcgis-experience-builder-customizing-and-extending-ds-europe-2023@2f581049ea0fad9a6b72bbe9bd6d0ecf955ffe66 🚀

--- cdn/1/widgets/slider-filter/dist/setting/translations/default.ts
+++ cdn/1/widgets/slider-filter/dist/setting/translations/default.ts
@@ -1,9 +1,21 @@
+/**
+ * Copyright 2023 ESRI (UK) Limited
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the Licence.
+ * A copy of the license is available in the repository's license.txt file.
+ */
+
 export default {
-  _widgetLabel: 'Slider Filter',
-  printPlaceholder: 'Select or switch to a webmap to enable slider filter.',
+  _widgetLabel: "Slider Filter",
+  printPlaceholder: "Select or switch to a webmap to enable slider filter.",
   //errors
-  chooseMapTip: 'Please select a map with 2D data.',
-  chooseWeightType: 'Please select whether to use AND or OR.',
-  SetWeights: 'Use the sliders to set the minimum visible values.',
-  selectMapWidget: 'Select Map widget'
-}
+  chooseMapTip: "Please select a map with 2D data.",
+  chooseWeightType: "Please select whether to use AND or OR.",
+  setWeights: "Use the sliders to set the minimum visible values.",
+  selectMapWidget: "Select Map widget",
+};
